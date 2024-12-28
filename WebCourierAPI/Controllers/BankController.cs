db6f5a0092853ba1c03c89421c09630625de9fe7
habiburrahman1836 <habibrahmancmt@gmail.com>
2024-12-29 01:20:20 +0600

    Bank Controller Create

---
+++ WebCourierAPI/Controllers/BankController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebCourierAPI.Attributes;
+using WebCourierAPI.Models;
+
+namespace WebCourierAPI.Controllers
+{
+    [EnableCors("Policy1")]
+    [AuthAttribute("", "Companies")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankController : ControllerBase
+    {
+        //private readonly WebCorierApiContext _context;
+
+        //public BankController(WebCorierApiContext context)
+        //{
+        //    _context = context;
+        //}
+
+        // GET: api/Bank
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bank>>> GetBanks()
+        {
+            WebCorierApiContext _context = new WebCorierApiContext();
+            return await _context.Banks.ToListAsync();
+        }
+
+        // GET: api/Bank/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bank>> GetBank(int id)
+        {
+            WebCorierApiContext _context = new WebCorierApiContext();
+            var bank = await _context.Banks.FindAsync(id);
+
+            if (bank == null)
+            {
+                return NotFound();
+            }
+
+            return bank;
+        }
+
+        // POST: api/Bank
+        [HttpPost]
+        public async Task<ActionResult<Bank>> PostBank([FromBody] Bank bank)
+        {
+            WebCorierApiContext _context = new WebCorierApiContext();
+
+            if (bank == null || string.IsNullOrEmpty(bank.BranchName))
+            {
+                return BadRequest("BranceName is required");
+            }
+
+            var token = Request.Headers["Token"].FirstOrDefault();
+            var user = AuthenticationHelper.ValidateToken(token);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid or expired token.");
+            }
+
+            bank.CreateBy = user.UserName;
+            bank.CreateDate = DateTime.UtcNow;
+
+            _context.Banks.Add(bank);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBank), new { id = bank.BankId }, bank);
+        }
+
+        // PUT: api/Bank/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBank(int id,[FromBody] Bank bank)
+        {
+            WebCorierApiContext _context = new WebCorierApiContext();
+            if (id != bank.BankId)
+            {
+                return BadRequest("Mismatch Bank Id");
+            }
+            var existingBank = await _context.Banks.FindAsync(id);
+            if (existingBank == null) 
+            { 
+                return NotFound("Bank ID in not Found");
+            }
+            existingBank.BranchName = bank.BranchName;
+            
+            _context.Entry(existingBank).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BankExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        // DELETE: api/Bank/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBank(int id)
+        {
+            WebCorierApiContext _context = new WebCorierApiContext();
+            var bank = await _context.Banks.FindAsync(id);
+            if (bank == null)
+            {
+                return NotFound();
+            }
+
+            _context.Banks.Remove(bank);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BankExists(int id)
+        {
+            WebCorierApiContext _context = new WebCorierApiContext();
+            return _context.Banks.Any(e => e.BankId == id);
+        }
+    }
+}
